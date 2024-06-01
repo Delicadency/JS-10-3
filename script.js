81ca1a468a8fffea0625b92fa3c8707579d3da20
Agata Kamińska <a.fluffypie@gmail.com>
2024-06-01 22:50:56 +0200

    created div, added options and styles

--- script.js
+++ script.js
@@ -2,12 +2,22 @@
 
 Zwrócony zostanie obiekt z listą dostępnych walut i ich wartości względem EUR. Twoim zadaniem jest stworzyć w JS element <select> i włożyć do niego <option>, które będą miały w sobie tekst odpowiadający kodom walut, a oprócz tego atrybut value ustawiony na taką samą wartość.
 Stworzony <select> dodaj do body. */
+const createContainer = () => {
+  const div = document.createElement("div");
+  div.classList.add("container");
+  div.classList.add("flex-center");
+  document.body.appendChild(div);
+  return div;
+};
+const container = createContainer();
 
 const createButton = () => {
   const button = document.createElement("button");
   button.id = "getCurrencies";
   button.innerText = "Pobierz waluty";
-  document.body.appendChild(button);
+  button.classList.add("margin");
+  button.classList.add("padding");
+  container.appendChild(button);
   return button;
 };
 const getCurrencyList = createButton();
@@ -15,14 +25,25 @@ getCurrencyList.addEventListener("click", () => {
   const apiURL = "https://api.frankfurter.app/latest";
   fetch(apiURL)
     .then((response) => response.json())
-    .then((data) => {console.log(data);
-        const rates = data?.[0]?.rates;
-        if (rates){
-            const select = document.body.createElement("select");
-            select.id = "currency-select";
-            document.body.appendChild(select);
-
+    .then((data) => {
+      const rates = data.rates;
+      if (rates) {
+        const label = document.createElement("label");
+        label.setAttribute("for", "currency-select");
+        label.innerText = "Wybierz walutę";
+        label.classList.add("margin");
+        container.appendChild(label);
+        const select = document.createElement("select");
+        select.id = "currency-select";
+        for (const [currency] of Object.entries(rates)) {
+          const option = document.createElement("option");
+          option.value = currency;
+          option.innerText = currency;
+          option.classList.add("padding");
+          select.appendChild(option);
         }
+        container.appendChild(select);
+      }
     })
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 });
