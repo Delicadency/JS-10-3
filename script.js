efca522147404d09a0c5d1d0d632383e9c501e29
Agata Kamińska <a.fluffypie@gmail.com>
2024-06-01 21:15:17 +0200

    added comment

---
+++ script.js
@@ -0,0 +1,4 @@
+/* Korzystając z Frankfurter API, stwórz przycisk o id=”getCurrencies”, który wywołuje funkcję getCurrencyList, która wykona fetch wywołującą zapytanie pod adres https://api.frankfurter.app/latest. 
+
+Zwrócony zostanie obiekt z listą dostępnych walut i ich wartości względem EUR. Twoim zadaniem jest stworzyć w JS element <select> i włożyć do niego <option>, które będą miały w sobie tekst odpowiadający kodom walut, a oprócz tego atrybut value ustawiony na taką samą wartość.
+Stworzony <select> dodaj do body. */
